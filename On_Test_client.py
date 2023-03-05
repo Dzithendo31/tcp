63f5c5c12c335347ba04285af9027eb1ab95b0ee
Dzithendo Mathegu <113150837+Dzithendo31@users.noreply.github.com>
2023-03-05 20:58:08 +0200

    Update On_Test_client.py

--- On_Test_client.py
+++ On_Test_client.py
@@ -1,55 +1,74 @@
-from http import client
+from curses import window
 import socket
+import time
 import hashlib
 import os
 import ssl
+from tkinter import filedialog
 
 IP = socket.gethostbyname(socket.gethostname())
-Port = 1133
+Port = 4001
 add = (IP, Port)
 FORMAT = "utf-8"
 SIZE = 1024
 buffer = 1024
+#
 
 
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(add)
+    try:
+        F_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        context = ssl.create_default_context()
+        client = context.wrap_socket(F_client, server_hostname=IP)
+        client.connect(add)
+    except ssl.SSLError as e:
+        print("Connection to user Failed")
+        return
     #now the connection has been established
-
-    #use of SLL to wrap the socket  with SLL
-    #context = ssl.create_default_context()
-    #client = context.wrap_socket(TCP_client, server_hostname="localhost")
     while True:
         cmd = ""
-        cmd = input("enter command to send to server:\n'd' download\n'u' Upload\n'l' To list files\n'q' to Quit\n " )
+        cmd = input("enter command to send to server:\n'd' download\n'u' Upload\n'X' delete file\n'l' To list files\n'q' to Quit\n " )
 
         #command, file_name = cmd.split()
         if cmd == "u" or cmd == "U":
             
-            file_name = input("Enter file_name:")
-            state = input("closed 'C' or open 'O':")
-            if state == "O" or state == "o":
-                client.send(f"upload/{file_name}/O".encode(FORMAT))
-            if state == "C" or state == "c":
+            #file_name = input("Enter file_name:")
+            file_name = filedialog.askopenfilename()
+            path = file_name.split("/")
+            file_name = path[-1]
+            state = input("Would you like to protect the file(Y/N)")
+            if state == "N" or state == "n":
+                client.send(f"upload%{file_name}%O".encode(FORMAT))
+            elif state == "Y" or state == "y":
                 pin = input("Enter code to Lock file:")
-                client.send(f"upload/{file_name}/C@{pin}".encode(FORMAT))
+                client.send(f"upload%{file_name}%C@{pin}".encode(FORMAT))
+            else:
+                print("Incorrect input.")
+                continue
             try:
+                #before upload changefile name if duplicate
+                #so client waits for message from server of how the file is saved.
+                msg = client.recv(64).decode(FORMAT)
+                print(f"[SEVER]:\n{msg}")
                 upload(client,file_name)
+
             except:
                 print("Error has occured, restart App.")
                 continue
         elif cmd == "l" or cmd == "L":
             #client.send("query/none/o@000".encode(FORMAT))
             #the program will then recieve a long string of all the Files
-            client.send("query/none/o@000".encode(FORMAT))
+            client.send("query%none%000".encode(FORMAT))
             msg = client.recv(SIZE).decode(FORMAT)
-            print(f"[SEVER]:\n{msg}")
-        elif cmd == "d" or cmd == "Q":
+            print("{:25}".format("Name") + "  State  Size")
+            print(msg)
+
+        elif cmd == "d" or cmd == "D":
+            
             filename = input('Enter filename: ')
-            pin = input('Enter File key, 0 if none: ')
-            client.send(f"download/{filename}/{pin}".encode(FORMAT))
+            pin = input('Enter File password, 0 if none: ')
+            client.send(f"download%{filename}%{pin}".encode(FORMAT))
             
             
             msg = client.recv(SIZE).decode(FORMAT)
@@ -58,7 +77,23 @@ def main():
             #this will either be Sending or NOT Found
             #but before it downloads it has to check
             else:
-                download(filename,client)
+                try:
+                    download(filename,client)
+                except:
+                    print("Error from server.")
+                    continue
+        elif cmd == "X" or cmd == "x":
+            filename = input('Enter filename: ')
+            pin = input('Enter File key, 0 if none: ')
+            client.send(f"delete%{filename}%{pin}".encode(FORMAT))
+            msg = client.recv(SIZE).decode(FORMAT)
+            if msg == "NONE":
+                print("File not found or Incorrect Key for File Delete")
+            else:
+                print(f"File {filename} deleted")
+            #wait for message and break
+
+        
         else:
             "Incorrect Input, connection Lost"
             client.close()
@@ -67,19 +102,13 @@ def main():
 
 def upload(client,file_name):
 
-    #client.send("learn.txt".encode(FORMAT))
-    
-    #this part waits for the message of FileName recieved
-    msg = client.recv(SIZE).decode(FORMAT)
-    print(f"[SEVER]: {msg}")
-
     #get file size
     file_size = os.path.getsize(file_name)
+    #2A
     client.send(str(file_size).encode(FORMAT))
-    print(file_size)
     sent = 0 #bytes
 
-    print("up;loading")
+    print("uploading......")
     with open(file_name, 'rb') as f:
 
         while sent<file_size:
@@ -100,45 +129,46 @@ def upload(client,file_name):
             else:
                 print("File disturedbed")
                 break
-            # we use sendall to assure transimission in 
-            
-
-    #send signal than its done
-    #client.send(data.encode(FORMAT))
-    #msg = client.recv(SIZE).decode(FORMAT)
-    #print(f"[SEVER]: {msg}")
-    #Creating and Sending A hash to check if file altered.
-    #file = open(file_name, "rb")
-    #byte = file.read()
-    
-    #file_hash = hashlib.sha256(byte).hexdigest()
-    #print(file_hash)
-    #client.sendall(file_hash.encode())
-
-    #ms = client.recv(SIZE).decode(FORMAT)
-    #print(f"[SEVER]: {ms}")
-
+        #just for message controls 
+        control = client.recv(SIZE).decode(FORMAT)
 
 def download(file_name,client):
 
-        print("[RECV] Filename received")
-        #file = open(f"data/{file_name}", "wb")
-        client.send("Filename received".encode(FORMAT))
+        print("Choose directory to save at")
+        #ask user to choose directory to save at
+        save_path = filedialog.askdirectory()
+        client.send("Send File".encode(FORMAT))
 
         SizeX = int(client.recv(SIZE).decode(FORMAT))
         #data = bytes(data, encoding='utf-8')
         sent = 0
-        with open(file_name, 'wb') as f:
+        with open(f"{save_path}/{file_name}", 'wb') as f:
             
+            print("Downloading....at 14mbps")
+            start_time = time.time()
             while sent<SizeX:
                 data = client.recv(1096)#.decode(FORMAT)
-                sent += 1096
-                f.write(data)
+                
+                computed_file_hash = hashlib.sha256(data).hexdigest()
+                hash = client.recv(64).decode(FORMAT)
+                if hash == computed_file_hash:
+                    sent += 1096
+                    f.write(data)
+                    client.send("Continue".encode(FORMAT))
+                else:
+                    #delete file
+                    os.remove(f"data/{file_name}")
+                    client.send("Corrupt".encode(FORMAT))
+                    break
 
             f.close()
-        
-        print("[RECV] Filename Data received")
+
+        print("[CLIENT] Filename Data received")
         client.send("File data received".encode(FORMAT))
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        #print("Transmisson Rate: "+elapsed_time +" seconds")
+
 
 if __name__ == '__main__':
     main()
